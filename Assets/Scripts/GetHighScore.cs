b1b72461c1f419bf9f125ffb4a5f62e7ff608de6
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-10-19 21:31:29 -0400

    GetHighScore Script
    *Added Script to access high score player pref

---
+++ Assets/Scripts/GetHighScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetHighScore : MonoBehaviour
+{
+    public GameObject[] scoreArray;
+    public void Start() {
+        for (int i = 0; i < 4; i++)
+        {
+            Text gt = scoreArray[i].GetComponent<Text>();
+            gt.text = "Score lvl " + i + ": " + PlayerPrefs.GetInt(i + "HighScore");
+            //scoreArray[i].GetComponet<Text>() = "Score lvl " +i + ": " + PlayerPrefs.GetInt(i + "HighScore");
+
+            //     Text gt = this.GetComponent<Text>();
+            //gt.text = "Score lvl " + MissionDemolition.getLevel() + ": " + (score - (int)time);
+            //PlayerPrefs.GetInt(i + "HighScore");
+        }
+    }
+}
