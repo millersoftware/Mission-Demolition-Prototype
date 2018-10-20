08678865ecea77639f3a62cdde644f7b6bcfa747
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-10-19 20:59:54 -0400

    Time Calculation for High Score
    *Added time to HighScore code

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HighScore : MonoBehaviour {
-    static public int score = 0;
+    static public int score = 1000;
+    public float time = 0.0f;
+    public int level = 0;
 	// Use this for initialization
 	void Awake () {
-		if(PlayerPrefs.HasKey("High Score"))
+        if (PlayerPrefs.HasKey("High Score"))
         {
             score = PlayerPrefs.GetInt("HighScore");
         }
@@ -19,12 +21,17 @@ public class HighScore : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        time = Time.time;
+        if (level != MissionDemolition.getLevel())
+        {
+            score = 1000;
+        }
         Text gt = this.GetComponent<Text>();
-        gt.text = "High Score lvl " + MissionDemolition.getLevel() + ": " + score;
+        gt.text = "Score lvl " + MissionDemolition.getLevel() + ": " + (score - (int)time);
 
         if (score > PlayerPrefs.GetInt(MissionDemolition.getLevel() +"HighScore"))
         {
-            PlayerPrefs.SetInt(MissionDemolition.getLevel() + "HighScore", score);
+            PlayerPrefs.SetInt(MissionDemolition.getLevel() + "HighScore", score - (int)time);
         }
 	}
 }
