3653a48469cacdbdef0a6bdc6b780aa5bf047acf
millersoftware <37933030+millersoftware@users.noreply.github.com>
2018-10-19 20:12:26 -0400

    HighScore Code Template
    *Added HighScore code

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+    static public int score = 0;
+	// Use this for initialization
+	void Awake () {
+		if(PlayerPrefs.HasKey("High Score"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            PlayerPrefs.SetInt((string)i + "HighScore", score);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(score > PlayerPrefa.GetInt((string)MissionDemolition.level +"HighScore"))
+        {
+            PlayerPrefs.SetInt((string)MissionDemolition.level + "HighScore", score);
+        }
+	}
+}
